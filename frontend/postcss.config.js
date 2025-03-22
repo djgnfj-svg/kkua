fa11795640d425a5aed1662fe1384ec7fe47aac1
b-hyoung <youqlrqod@gmail.com>
2025-03-22 14:13:08 +0900

    setUp tailwindCss

---
+++ frontend/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+      require('@tailwindcss/postcss'), // ✅ 새로운 플러그인 추가
+      require('autoprefixer'),
+  ]
+}
---
+++ frontend/src/Pages/Lobby/Lobby.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Lobby() {
+
+  return (
+    <div>
+      <div className="text-xl bg-blue-500">asdsadasd</div>
+      <div></div>
+      <div id='lobby__footbar'></div>
+    </div>
+  )
+}
+
+export default Lobby
