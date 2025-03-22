7d946635ca7d709c009338ec18ed941db5be153b
b-hyoung <youqlrqod@gmail.com>
2025-03-22 12:02:20 +0900

    fix node -version & setUp frontend

---
+++ frontend/src/Api/userApi.js
@@ -0,0 +1,7 @@
+/*
+export const USER_API = {
+  GET_USER: '/api/user',
+  CREATE_USER: '/api/user/create',
+  UPDATE_USER: '/api/user/update',
+};
+*/
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; //루트
+
+// Url별 화면 표시
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />         첫 화면 */}
+          {/* <Route path="/lobby" element={<About />} />   로비 화면 */}
+          {/* <Route path="/keaing" element={} />           인게임 화면 */}
+        </Routes>
+      </Router>
     </div>
   );
 }
