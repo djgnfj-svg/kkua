2424bee48b7d4869f1f339ef83af580107417ba3
b-hyoung <youqlrqod@gmail.com>
2025-03-23 16:27:06 +0900

    fix App.js pathUrl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           {/* <Route path="/" element={<Home />} />         첫 화면 */}
-          <Route path="/lobby" element={<Lobby />} />   {/* 로비 화면 */}
+          {/* <Route path="/lobby" element={<Lobby />} />   로비 화면 */}
           {/* <Route path="/keaing" element={} />           인게임 화면 */}
         </Routes>
       </Router>
