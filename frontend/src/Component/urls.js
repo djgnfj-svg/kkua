2ac00f35f5e6b4e4e18766cb5ae9aae7758a4df7
b-hyoung <youqlrqod@gmail.com>
2025-04-14 02:37:33 +0900

    fixed lobbyurls

--- frontend/src/Component/urls.js
+++ frontend/src/Component/urls.js
@@ -1,3 +1,4 @@
 export const homUrl = '/';
-export const lobbyUrl = (id) => `/kealobby/${id}`
+// export const lobbyUrl = (id) => `/kealobby/${id}`
+export const lobbyUrl = `/lobby`
 export const gameUrl = '/keaing'
