f662c308c05a55e4fb75264ac6b65996f396d10d
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2025-04-16 16:32:19 +0900

    Feat game page logic (#22)
    * fix . Docker-ComposeUp(delete nodemodule
    
    * fix slide(Color) => slide(img)
    
    * slide action
    
    * add empty room
    
    * add empty roomBox
    
    * remove 불필요한 주석
    
    * fixed LobbyUI
    
    * fix lobby ImgURL
    
    * add AddIcon & fix iconUrl
    
    * delete paste Text & add banerImg
    
    * delete space (index.html , Lobby.css
    
    * add navigate Ingame
    
    * add lobbyBtn Navigate
    
    * add Modal
    
    * add modal UI
    
    * fix modal UI
    
    * fix add bunny room UI
    
    * add Logic Top Messege
    
    * InGame 작업 중간 저장
    
    * add timer - timeout
    
    * add ingame tock check
    
    ---------
    
    Co-authored-by: b-hyoung <youqlrqod@gmail.com>

---
+++ frontend/src/InGame/MsgData.js
@@ -0,0 +1,15 @@
+// src/InGame/MsgData.js
+const msgData = [
+    { word: '햄스터', desc: '쥐과 동물이다' },
+    { word: '터널', desc: '' },
+    { word: '널뛰기', desc: '' }, 
+    { word: '기분', desc: '' },
+    { word: '기러기', desc: '' },
+    { word: '기형', desc: '' },
+    { word: '기미', desc: '' },
+    { word: '분노', desc: '' },
+    { word: '노래', desc: '' }
+  ];
+  
+export default msgData;
+  
